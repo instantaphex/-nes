ddeee621c5ec30edb4560f7a88ac57a54afd5086
John Bryan <jb@johns-mbp.attlocal.net>
2018-01-28 16:16:03 -0600

    removed unneeded comments

--- variables.asm
+++ variables.asm
@@ -4,14 +4,11 @@ buttons2 .rs 1 ; conroller variable for player 1
 bg_ptr_low .rs 1 ; bg pointer low byte
 bg_ptr_hi .rs 1 ; bg pointer high byte
 
-player_x .rs 1 ; player x position
-player_y .rs 1 ; player y position
+player_x .rs 1
+player_y .rs 1
 
-player_vel_x .rs 1 ; x velocity
-player_vel_y .rs 1 ; x velocity
-
-player_accel_x .rs 1 ; x velocity
-player_accel_y .rs 1 ; x velocity
+player_speed_x .rs 1
+player_speed_y .rs 1
 
 player_direction .rs 1 ; facing left or right (0 - right, 1 - left)
 
